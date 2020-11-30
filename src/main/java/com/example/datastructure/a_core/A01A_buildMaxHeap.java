1a6e5a97092b8bd1942d4dab396f18b3b6a3afd9
winkymonkey <pal.abhishek.2008@gmail.com>
2020-12-01 03:36:15 +0530

    test

--- src/main/java/com/example/datastructure/a_core/A01A_buildMaxHeap.java
+++ src/main/java/com/example/datastructure/a_core/A01A_buildMaxHeap.java
@@ -6,15 +6,24 @@ import java.util.Arrays;
  * ************************************************************************************
  * A given array may not have all its elements in Heap structure.
  * To convert an array to Max Heap we need some algorithm where 'max_heapify()' is the heart of it.
- * 
  * ************************************************************************************
  */
 
 public class A01A_buildMaxHeap {
 
 	public static void main(String[] args) {
-		int A[] = { 1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17 };
-		//index		0  1  2  3  4  5   6   7  8  9   10
+		int A[] = { 1, 14, 10, 8, 7, 9, 3, 2, 4, 6 };
+		//index		0  1   2   3  4  5  6  7  8  9
+		
+		/*
+		 *         1
+		 *       /   \
+		 *     14     10
+		 *    /  \    / \
+		 *   8    7  9   3
+		 *  / \  /
+		 * 2   4 6
+		 */
 		
 		buildMaxHeap(A);
 	}
@@ -22,24 +31,24 @@ public class A01A_buildMaxHeap {
 	
 	private static void buildMaxHeap(int A[]) {
 		int n = A.length;
-		for (int i = (n/2)-1; i >= 0; i--) { 			// non-leaves are from 0 to Floor(n/2)-1
-			max_heapify(A, i); 							// leaves are from Floor(n/2) to n
+		for (int i = (n/2)-1; i >= 0; i--) { 			// non-leaves are from 0 to Floor(length/2)-1
+			max_heapify(A, i); 							// leaves are from Floor(length/2) to length-1
 		}
 		System.out.println(Arrays.toString(A));
 	}
 	
 	
 	private static void max_heapify(int A[], int i) {
-		int n = A.length;
+		int heapsize = A.length;
 		
 		int largest = i;
 		int l = 2*i + 1;
 		int r = 2*i + 2;
 
-		if (l < n && A[l] > A[i])
+		if (l < heapsize && A[l] > A[i])		//if l exists and l element > root element
 			largest = l;
 
-		if (r < n && A[r] > A[largest])
+		if (r < heapsize && A[r] > A[largest])	//if r exists and r element > largest element
 			largest = r;
 
 		if (largest != i) {
